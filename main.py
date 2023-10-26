f68fddc86af7b66952d17cd1dc7f3bb9f340e2b1
Andythompson3 <thompson.andrew731@gmail.com>
2023-10-26 11:21:06 -0600

    added random gen function
    I added a small snippet of code that allows users to tell the app how long they would like the the quiz to be and give them random numbers list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from func_list import *
 def main():
     on = True
     while on:
-        print("Hello, welcome to our quiz application, please select on of the following options")
+        print("Hello, welcome to are you smarter than a 5th grade, please select on of the following options")
     
         print("1. Add a user")
         print("2. Take a quiz")
@@ -21,7 +21,11 @@ def main():
         elif user_input == 3:
             pass
         elif user_input == 2:
-            pass
+            print("Welcome to are you smarter than a 5th grade")
+            quiz_size = int(input("How many questions would you like your quiz to have?"))
+            quiz_num = rand_num(quiz_size)
+
+                
         elif user_input == 1:
             username = input("What is your username?: ")
             print(add_user(username))
