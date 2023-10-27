cad6484ad5db70099d1b27af24d981ace514cc82
Andythompson3 <thompson.andrew731@gmail.com>
2023-10-27 08:07:23 -0600

    Adding notes and reorganize if statement

--- main.py
+++ main.py
@@ -8,29 +8,37 @@ def main():
         print("2. Take a quiz")
         print("3. update user")
         print("4. Exit application")
+
         user_input = input("What is your option?: ")
+        # checks if the users input is a number or not
         if user_input.isnumeric():
-            user_input = int(user_input)
-            
+            user_input = int(user_input) 
         else:
             print("Input contains characters.")
             continue
 
-        if user_input == 4:
-            on = False
-        elif user_input == 3:
-            pass
+
+
+
+            #  Add a user
+        if user_input == 1:
+            username = input("What is your username?: ")
+            print(add_user(username))
+            
+            # Take a quiz
         elif user_input == 2:
             print("Welcome to are you smarter than a 5th grade")
             quiz_size = int(input("How many questions would you like your quiz to have?"))
             quiz_num = rand_num(quiz_size)
-
-                
-        elif user_input == 1:
-            username = input("What is your username?: ")
-            print(add_user(username))
             
-            
+            # update user
+        elif user_input == 3:
+            pass
+        
+            # Exit application
+        elif user_input == 4:
+            on = False
+         
         else:
             print("Sorry that is not a valid option")
     
