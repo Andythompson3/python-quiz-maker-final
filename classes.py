a0981f607d5c5c0a4f03d162718d1bd0d3649d1f
Andythompson3 <thompson.andrew731@gmail.com>
2023-11-01 18:55:22 -0600

    Delete file

--- classes.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    #This is the class that is going to create the user.
-    def __init__(self, username, name, surname, birthdate, telephone, email):
-        self.username = username
-        self.first_name = name
-        self.last_name = surname
-        self.birthday = birthdate
-        self.phone_number = telephone
-        self.email = email
